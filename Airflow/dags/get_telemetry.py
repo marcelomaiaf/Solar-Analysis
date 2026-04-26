59a8cb3879cfb530a722c0c4881054c83a81bb8b
marcelomaiaf <marcelo.filho@sou.inteli.edu.br>
2026-04-26 11:48:27 -0300

    Updating SQL query

--- Airflow/dags/get_telemetry.py
+++ Airflow/dags/get_telemetry.py
@@ -2,6 +2,14 @@ import datetime
 from airflow.sdk import DAG
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
+sql_query = """
+            select name,vendor,timezone, 
+                   capacity_kwp,location,address,
+                   latitude, longitude,azimuth_deg,tilt_deg	, 
+                   vendor_plant_id,vendor_device_id,vendor_timezone, vendor_metadata_json 
+            from plants p join vendor_plant_links v on p.id = v.plant_id where vendor = 'weg'
+            """
+
 with DAG(
     dag_id="telemtry_dag",
     start_date=datetime.datetime(2026, 1, 1),
@@ -11,7 +19,7 @@ with DAG(
     get_plant_data = SQLExecuteQueryOperator(
         task_id="get_plant_data",
         conn_id="railway_postgres",
-        sql="SELECT * FROM plants",
+        sql=sql_query,
     )
 
     get_plant_data
